3d32c55772375868ded82d7aedf21830af9cd74b
atsb <ats@ats>
2026-09-14 21:58:01 +0200

    fixing build

---
+++ source/Doom.h
@@ -0,0 +1,4 @@
+#ifndef DOOM3DO_CASE_DOOM_H
+#define DOOM3DO_CASE_DOOM_H
+#include "doom.h"
+#endif
---
+++ source/States.h
@@ -0,0 +1,4 @@
+#ifndef DOOM3DO_CASE_STATES_H
+#define DOOM3DO_CASE_STATES_H
+#include "states.h"
+#endif
